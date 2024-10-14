3ae60822409dcf6863d60a7083d097be6cec832f
Veloxal <127298642+axqqt@users.noreply.github.com>
2024-10-14 18:12:34 +0530

    yes

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.26",
+  solidity: '0.8.4',
   networks: {
     hardhat: {
       forking: {
